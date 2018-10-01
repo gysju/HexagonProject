1f59aad04923a43513da50027c8251231c3f39b5
unknown <gysenjulien@gmail.com>
2018-10-01 23:12:34 +0200

    inital commit

---
+++ Assets/Scripts/Hexa.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+
+public class Hexa : MonoBehaviour
+{
+    public const float OUTER_RADIUS = 1.0f;
+    public const float INNER_RADIUS = 0.866025404f;
+
+    public static Vector3 Corner_UP = new Vector3( 0.0f, 0.0f, OUTER_RADIUS);
+    public static Vector3 Corner_UP_LEFT = new Vector3( -INNER_RADIUS, 0.0f, 0.5f * OUTER_RADIUS);
+    public static Vector3 Corner_UP_RIGHT = new Vector3( INNER_RADIUS, 0.0f, 0.5f * OUTER_RADIUS);
+
+    public static Vector3 Corner_DOWN = new Vector3(0.0f, 0.0f, -OUTER_RADIUS);
+    public static Vector3 Corner_DOWN_LEFT = new Vector3(-INNER_RADIUS, 0.0f, -0.5f * OUTER_RADIUS);
+    public static Vector3 Corner_DOWN_RIGHT = new Vector3(INNER_RADIUS, 0.0f, -0.5f * OUTER_RADIUS);
+
+    [SerializeField] Shader _s;
+    Mesh _m;
+    MeshFilter _mf;
+    MeshRenderer _mr;
+
+    List<Vector3> _vertices = new List<Vector3>();
+    List<int> _triangles = new List<int>();
+
+    private void Awake()
+    {
+        _mr = GetComponent<MeshRenderer>();
+        if (_mr == null)
+            _mr = gameObject.AddComponent<MeshRenderer>();
+
+        _mf = GetComponent<MeshFilter>();
+        if (_mf == null)
+            _mf = gameObject.AddComponent<MeshFilter>();
+
+        if (_s == null)
+            Debug.LogError("No shader !");
+    }
+
+    void Start ()
+    {
+        generateMesh();	
+	}
+
+    void generateMesh()
+    {        
+        _m = new Mesh();
+
+        generateTriangle( transform.position, transform.position + Corner_UP_RIGHT, transform.position + Corner_DOWN_RIGHT);
+        generateTriangle( transform.position, transform.position + Corner_DOWN_RIGHT, transform.position + Corner_DOWN);
+        generateTriangle( transform.position, transform.position + Corner_DOWN, transform.position + Corner_DOWN_LEFT);
+        generateTriangle(transform.position, transform.position + Corner_DOWN_LEFT, transform.position + Corner_UP_LEFT);
+        generateTriangle(transform.position, transform.position + Corner_UP_LEFT, transform.position + Corner_UP);
+        generateTriangle(transform.position, transform.position + Corner_UP, transform.position + Corner_UP_RIGHT);
+
+        _m.vertices = _vertices.ToArray();
+        _m.triangles = _triangles.ToArray();
+
+        _mf.mesh = _m;
+        if (_mr.material == null && _s != null)
+            _mr.material = new Material(_s);
+    }
+
+    void generateTriangle( Vector3 v1, Vector3 v2, Vector3 v3)
+    {
+        int verticesCount = _vertices.Count;
+
+        _vertices.Add( v1 );
+        _vertices.Add( v2 );
+        _vertices.Add( v3 );
+
+        _triangles.Add(verticesCount);
+        _triangles.Add(verticesCount + 1);
+        _triangles.Add(verticesCount + 2);
+    }
+}
---
+++ Assets/Scripts/TilesManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TilesManager : MonoBehaviour
+{
+    [Range(1, 10)]
+    public int Size = 1;
+
+    [Range( 0.0f, 1.0f)]
+    public float DistanceBetweenTwoCells = 0.1f;
+    List<Hexa> hexas = new List<Hexa>();
+
+    void Start()
+    {
+        generateTiles();            
+    }
+
+    void generateTiles()
+    {
+        //up
+        int upSize = Size + 1;
+        for (int j = 1; j <= Size; j++)
+        {
+            for (int i = 0; i <= upSize; i++)
+            {
+                GameObject hexa = new GameObject("Hexa", typeof(Hexa));
+                hexa.transform.position = new Vector3( (Hexa.OUTER_RADIUS) * i - (Hexa.OUTER_RADIUS) * (upSize * 0.5f), 
+                                                       0.0f, 
+                                                       (Hexa.INNER_RADIUS) * j);
+            }
+            upSize--;
+        }
+
+        //center
+        for (int i = -Size; i < Size + 1; i++)
+        {
+            GameObject hexa = new GameObject("Hexa", typeof(Hexa));
+            hexa.transform.position = new Vector3( ( Hexa.OUTER_RADIUS) * i, 0.0f, 0.0f);
+        }
+
+        //down
+        int downSize = Size + 1;
+        for (int j = 1; j <= Size; j++)
+        {
+            for (int i = 0; i <= downSize; i++)
+            {
+                GameObject hexa = new GameObject("Hexa", typeof(Hexa));
+                hexa.transform.position = new Vector3( (Hexa.OUTER_RADIUS) * i - (Hexa.OUTER_RADIUS) * (downSize * 0.5f),
+                                                       0.0f,
+                                                       ( -Hexa.INNER_RADIUS) * j);
+            }
+            downSize--;
+        }
+    }
+}
